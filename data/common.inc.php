35db8fb27b73f499f77caf49cfd46d97e1be0ae1
sunjindeng <871183373@qq.com>
2020-07-01 09:34:27 +0800

    frist commit

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'rm-2ze7330anj61428oj.mysql.rds.aliyuncs.com';
+$cfg_dbname = 'adminbuy';
+$cfg_dbuser = 'canyinyunfu';
+$cfg_dbpwd = 'h4eGcJkJhOrj9AEy';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'gbk';
+
+
+?>
